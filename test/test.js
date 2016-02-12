eadea0d0d46fd2a6a9646afd096ac27f4c740ba6
Summer Wollin <summer.wollin@gmail.com>
2016-02-11 20:21:33 -0700

    Include tests for leapYear function, include tests for if divisible by 4 and if divisible by 100 but not 400

--- test/test.js
+++ test/test.js
@@ -1,6 +1,11 @@
 var code = require('../main');
 var expect = require('chai').expect;
 
-describe('', function() {
-  it('');
-});
+describe("leapYear", function() {
+  it("returns true if divisible by 4", function() {
+    expect(code.leapYear(2016)).to.equal(true)
+  })
+  it("returns false if divisible by 100 and not 400", function() {
+    expect(code.leapYear(1800)).to.equal(false)
+  })
+})
