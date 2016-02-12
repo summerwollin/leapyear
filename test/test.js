350bf93fa84fbc9d2c4061c4c4b99eac67fa3687
Summer Wollin <summer.wollin@gmail.com>
2016-02-12 12:02:09 -0700

    change tests to be more explicit

--- test/test.js
+++ test/test.js
@@ -2,10 +2,16 @@ var code = require('../main');
 var expect = require('chai').expect;
 
 describe("leapYear", function() {
-  it("returns true if divisible by 4", function() {
+  it("returns true for 2016", function() {
     expect(code.leapYear(2016)).to.equal(true)
   })
-  it("returns false if divisible by 100 and not 400", function() {
-    expect(code.leapYear(1800)).to.equal(false)
+  it("returns false for 2015", function() {
+    expect(code.leapYear(2015)).to.equal(false)
+  })
+  it("returns true for 1600", function() {
+    expect(code.leapYear(1600)).to.equal(true)
+  })
+  it("returns false for 1700", function() {
+    expect(code.leapYear(1700)).to.equal(false)
   })
 })
